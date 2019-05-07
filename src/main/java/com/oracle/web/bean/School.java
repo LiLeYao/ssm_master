db884c0e5aa6018f262c2f14cb2d318281110bbe
LiLeYao <LiLeYao@qq.com>
2019-05-07 10:25:24 +0800

    第一次提交ssm

---
+++ src/main/java/com/oracle/web/bean/School.java
@@ -0,0 +1,33 @@
+package com.oracle.web.bean;
+
+public class School {
+    private Integer schoolId;
+
+    private String name;
+
+    private String address;
+
+    public Integer getSchoolId() {
+        return schoolId;
+    }
+
+    public void setSchoolId(Integer schoolId) {
+        this.schoolId = schoolId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address == null ? null : address.trim();
+    }
+}
